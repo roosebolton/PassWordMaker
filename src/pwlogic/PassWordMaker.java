0dd641792357666b3161cf85dc0fb10e69f533ed
roosebolton <roosebolton@outlook.com>
2021-07-25 11:37:01 +0200

    "Some comment added. Get lunch"

--- src/pwlogic/PassWordMaker.java
+++ src/pwlogic/PassWordMaker.java
@@ -1,56 +1,71 @@
 package pwlogic;
 
 import java.util.concurrent.ThreadLocalRandom;
+/**
+*A class that generates random passwords of minimal length 8. With on/off options for: Special characters, numbers, ambigious characters.
+* @author roosebolton github.com/roosebolton
+ **/
 
 public class PassWordMaker {
-    //all chars non first
+
+    /////////////////////////Attributes///////////////////////
+
+    //Char array that excludes special characters that are better not used as first characters
     private char[] acnf =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','(',')','?','[',']','_','`','~',';',':','@','#','$','%','^','&','*','+','=','-','.'};
 
-    //all chars first
+    //Char array that includes special characters
     private char[] acf =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','!','(',')','?','[',']','_','`','~',';',':','@','#','$','%','^','&','*','+','='};
 
-    //non special characters
+    //Char array that does not include special characters
     private char[] nsc =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'};
 
-    //non special non num chars
+    //Char array that does not include special characters and numbers
     private char[] nsnnc  = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-    //allCharsNonAmbNonFirst
+    //Char array that excludes special characters that are better not used as first characters and exludes ambigious characters
     private char[] acnanf =  {'a','b','c','d','e','f','g','h','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','2','3','4','5','6','7','8','9','!','(',')','?','[',']','_','`','~',';',':','@','#','$','%','^','&','*','+','=','-','.'};
 
-    //allCharsNonAmbFirst
+    //Char array that includes special character and excludes special characters
     private char[] acnaf =  {'a','b','c','d','e','f','g','h','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','2','3','4','5','6','7','8','9','!','(',')','?','[',']','_','`','~',';',':','@','#','$','%','^','&','*','+','='};
 
-    //nonSpecNonAmbChars
+    //Char array that excludes spcecial characters and ambigious characters
     private char[] nsnac =  {'a','b','c','d','e','f','g','h','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','2','3','4','5','6','7','8','9'};
 
-    //nonSpecNonNumNoneAmbChars
+    //Char array that excludes special characters and numbers and ambigious characters
     private char[] nsnnnac  = {'a','b','c','d','e','f','g','h','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-    //all chars non first length
+    //Length of the acnf array
     private int acnflength = acnf.length;
 
-    //all chars first length
+    //aLength of the acf array
     private int acflength = acf.length;
 
-    //no chars length
+    //Length of the nsc array
     private int nsclength = nsc.length;
 
-    //nonSpecNonNumLength
+    //Length of the nsnnc array
     private int nsnnclength = nsnnc.length;
 
-    //allCharsNonAmbNonFirstlength
+    ///Length of the acnanf array
     private int acnanflength = acnanf.length;
 
-    //allCharsNonAmbFirstLength
+    ///Length of the acnaf array
     private int acnaflength = acnaf.length;
 
-    //nonSpecNonAmbCHarsLength
+    ///Length of the nsnac array
     private int nsnaclength = nsnac.length;
 
-    //nonSpecialNonNumberNonAmbLength
+    ///Length of the nsnnnac array
     private int nsnnaclength = nsnnnac.length;
+    /////////////////////////End of attributes///////////////////////
 
+    /////////////////////////Constructors///////////////////////
+
+    /**
+     *Empty constructor
+     **/
+    public PassWordMaker(){
+    }
     //checks if passwordlength is >8
     private boolean acceptableLength(int passwordlength)throws IllegalArgumentException{
         boolean result = true;
@@ -59,6 +74,8 @@ public class PassWordMaker {
         }
         return result;
     }
+    /////////////////////////End of Constructors///////////////////////
+
 
 
     private char[] allCharPassword(int pwlength)throws IllegalArgumentException{
